0bf65b6d0ff44d581e5ed2fef91108c6d7265f4d
Eiryn <eiryn@live.co.uk>
2024-12-07 22:05:47 +0000

    attempt UI

--- Assets/Scripts/CollectedItems.cs
+++ Assets/Scripts/CollectedItems.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine;
 
 
 public class CollectedItems : MonoBehaviour
 {
-    private List<GameObject> collectedObjects = new List<GameObject>();
+    public GameObject items;
+    public static bool isHidden = false;
 
+    private List<GameObject> collectedObjects = new List<GameObject>();
+   
     public void Add(GameObject s)
     {
         collectedObjects.Add(s);
     }
 
+    void Start()
+    {
+        isHidden = false;
+
+        items.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (collectedObjects.Count <= 0)
+        {
+            if
+            itemsHidden();
+        }
+        else
+        {
+            itemsShown();
+        }
     }
 }
